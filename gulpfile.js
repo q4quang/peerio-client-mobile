d7eacf514314968e5b7540a2d94b6ee2fd47d1c7
Anri Asaturov <anri82@gmail.com>
2015-07-30 09:58:34 +0300

    Migration to new api

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sh = require('shelljs');
-var xeditor = require("gulp-xml-editor");
+var fs = require('fs');
+var xeditor = require('gulp-xml-editor');
 var semver = require('semver');
 var react = require('gulp-react');
 var browserSync = require('browser-sync').create();
-var inquirer = require("inquirer");
+var inquirer = require('inquirer');
+var _ = require('lodash');
+var minimist = require('minimist');
 
+// extracting --cli --parameters
+var knownOptions = {
+  boolean: 'api',
+  default: {  }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+// put all the paths here
 var paths = {
   sass_main: ['scss/app.scss'],
   sass_all: ['scss/*.scss'],
@@ -18,7 +30,8 @@ var paths = {
   js: ['www/js/**/*.js'],
   jsx_src: 'jsx/*.jsx',
   jsx_dst: 'www/js/compiled_jsx',
-  config_xml: 'config.xml'
+  config_xml: 'config.xml',
+  peerio_client_api: 'bower_components/peerio-client-api/dist/*.js'
 };
 /*eslint-enable*/
 
@@ -27,18 +40,19 @@ gulp.task('compile', ['sass', 'jsx']);
 
 gulp.task('help', function () {
   console.log();
-  console.log('+------------------------------------------------------------------------------------+');
-  console.log('|                                 =====  USAGE  =====                                |');
-  console.log('+------------------------------------------------------------------------------------+');
-  console.log('| gulp serve       - start http server with live reload                              |');
-  console.log('| gulp compile     - same as "gulp sass jsx"                                         |');
-  console.log("| gulp run-android - compile + 'cordova run android'                                 |");
-  console.log("| gulp run-ios     - compile + 'cordova run ios'                                     |");
-  console.log('| gulp sass        - compile scss files                                              |');
-  console.log('| gulp jsx         - compile jsx files                                               |');
-  console.log('| gulp bump        - interactively bump app version in config.xml                    |');
-  console.log('| gulp version     - prints current(last) app version from config.xml                |');
-  console.log('+------------------------------------------------------------------------------------+');
+  console.log('+---------------------------------------------------------------------------------------+');
+  console.log('|                                 =====  USAGE  =====                                   |');
+  console.log('+---------------------------------------------------------------------------------------+');
+  console.log('| gulp serve       - start http server with live reload                                 |');
+  console.log('| gulp serve --api - start http server with live reload and watch symlinked peerio api  |');
+  console.log('| gulp compile     - same as "gulp sass jsx"                                            |');
+  console.log("| gulp run-android - compile + 'cordova run android'                                    |");
+  console.log("| gulp run-ios     - compile + 'cordova run ios'                                        |");
+  console.log('| gulp sass        - compile scss files                                                 |');
+  console.log('| gulp jsx         - compile jsx files                                                  |');
+  console.log('| gulp bump        - interactively bump app version in config.xml                       |');
+  console.log('| gulp version     - prints current(last) app version from config.xml                   |');
+  console.log('+---------------------------------------------------------------------------------------+');
   console.log();
 });
 
@@ -62,8 +76,26 @@ gulp.task('jsx', function () {
     .pipe(gulp.dest(paths.jsx_dst));
 });
 
-// start http server with live reload
+// starts http server with live reload
+// sass an jsx files are watched and compiled to www
+// peerio-api-client repository is watched if --api option is provided and copied to www too
+// http server watches www folder and reloads
 gulp.task('serve', ['compile'], function () {
+
+  // watching symlinked peerio-client-api package
+  if(options.api){
+    // watch triggers for every file, so we debounce it
+    var copyApi = _.debounce(function(){
+      sh.exec('bower-installer');
+    }, 1500);
+
+    copyApi();
+
+    gulp.watch(paths.peerio_client_api, copyApi);
+
+  }
+
+  // starting http server with watcher
   browserSync.init({
     notify: false,
     files: [paths.html, paths.css_src, paths.js],
@@ -82,15 +114,18 @@ gulp.task('serve', ['compile'], function () {
     }
   });
 
+  // compile watchers
   gulp.watch(paths.sass_all, ['sass']);
   gulp.watch(paths.jsx_src, ['jsx']);
+
 });
 
-gulp.task('run-android', ['compile'], function(){
+
+gulp.task('run-android', ['compile'], function () {
   sh.exec('cordova run android');
 });
 
-gulp.task('run-ios', ['compile'], function(){
+gulp.task('run-ios', ['compile'], function () {
   sh.exec('cordova run ios');
 });
 
@@ -112,15 +147,15 @@ gulp.task('bump', ['version'], function () {
       }
     ], function (answer) {
       if (answer.version === '[cancel]') return;
-      if(answer.version.indexOf('PATCH')===0){
+      if (answer.version.indexOf('PATCH') === 0) {
         bump('patch');
         return;
       }
-      if(answer.version.indexOf('MINOR')===0){
+      if (answer.version.indexOf('MINOR') === 0) {
         bump('minor');
         return;
       }
-      if(answer.version.indexOf('MAJOR')===0){
+      if (answer.version.indexOf('MAJOR') === 0) {
         bump('major');
         return;
       }
@@ -154,3 +189,4 @@ function bump(version) {
     }))
     .pipe(gulp.dest('.'));
 }
+
