570f33959a8ced870943d71169cff44816a94445
Anri Asaturov <anri82@gmail.com>
2015-08-22 01:19:46 +0300

    'Back' button disabled at root

--- jsx/footer.jsx
+++ jsx/footer.jsx
@@ -7,21 +7,16 @@
   Peerio.UI.Footer = React.createClass({
     mixins: [ReactRouter.Navigation, ReactRouter.State],
     //--- REACT EVENTS
-    getInitialState: function () {
-      return {};
-    },
-    componentWillMount: function(){
-    },
-    componentDidMount: function () {
-    },
-    componentWillUnmount: function () {
+    topLevelPaths: ['/app/tabs/messages','/app/tabs/files','/app/tabs/contacts'],
+    isTopLevel: function(){
+      return this.topLevelPaths.indexOf(this.getPath()) >= 0;
     },
     //--- RENDER
     render: function () {
 
       return (
         <div id="footer">
-          <div id="global-back" className={true ? '' : 'hide'}
+          <div id="global-back" className={this.isTopLevel() ?  'hide' : ''}
             onTouchEnd={this.goBack}>
             <i className="fa fa-chevron-left"></i>&nbsp;back
           </div>
