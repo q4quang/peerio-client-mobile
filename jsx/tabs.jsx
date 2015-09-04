4b027e3c6dad299c93e61922d095c47b42a33dba
Jan Drewniak <jan.drewniak@gmail.com>
2015-09-04 13:17:12 +0200

    animations between tabs #105

--- jsx/tabs.jsx
+++ jsx/tabs.jsx
@@ -1,12 +1,28 @@
 (function () {
   'use strict';
+
+  var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
   // Main component, entry point for React app
   Peerio.UI.Tabs = React.createClass({
+    mixins: [ReactRouter.Navigation, ReactRouter.State],
+    getInitialState: function(){
+      return {currentPath: this.getPath(), reverseAnimation:false}
+    },
+    componentWillReceiveProps: function(){
+      var previousPath = this.state.currentPath;
+      var currentPath = this.getPath();
+      var tabOrder = ['/app/tabs/messages', '/app/tabs/files', '/app/tabs/contacts'];
+      var back = tabOrder.indexOf(currentPath) < tabOrder.indexOf(previousPath);
+      this.setState({currentPath: currentPath, reverseAnimation: back })
+    },
     render: function () {
       return (
         <div>
           <Peerio.UI.TabBar/>
-          <RouteHandler/>
+          <ReactCSSTransitionGroup transitionName={this.state.reverseAnimation ? "page-animate-reverse": "page-animate"}>
+            {React.cloneElement(<RouteHandler/>, { key: this.state.currentPath })}
+          </ReactCSSTransitionGroup>
         </div>
       )
     }
