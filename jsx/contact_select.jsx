ec8018cf55218eab7db126956e428dad24f1bf10
Jan Drewniak <jan.drewniak@gmail.com>
2015-09-11 18:11:10 +0200

    adding intro screen to contact selector, for when users haven't added contacts yet.

--- jsx/contact_select.jsx
+++ jsx/contact_select.jsx
@@ -2,6 +2,7 @@
   'use strict';
 
   Peerio.UI.ContactSelect = React.createClass({
+    mixins: [ReactRouter.Navigation],
     getInitialState: function () {
       return {selection: this.props.preselected || []};
     },
@@ -26,15 +27,28 @@
         var isSelected = this.state.selection.indexOf(c.username) >= 0 ;
 
         contacts.push(
-          <Peerio.UI.Tappable onTap={this.toggle.bind(this, c.username)} key={c.username}>
-            <li className={isSelected ? 'contact selected' : 'contact'}>
-              <span type="checkbox" className={isSelected ? 'checkbox-input checked' : 'checkbox-input' }></span>
-              <Peerio.UI.Avatar username={c.username}/> {c.fullName}
-              <span className="username">({c.username})</span>
-            </li>
-          </Peerio.UI.Tappable>
+            <Peerio.UI.Tappable onTap={this.toggle.bind(this, c.username)} key={c.username}>
+              <li className={isSelected ? 'contact selected' : 'contact'}>
+                <span type="checkbox" className={isSelected ? 'checkbox-input checked' : 'checkbox-input' }></span>
+                <Peerio.UI.Avatar username={c.username}/> {c.fullName}
+                <span className="username">({c.username})</span>
+              </li>
+            </Peerio.UI.Tappable>
         )
       }.bind(this));
+
+        if (Peerio.user.contacts.length === 1) {
+          var intro_content = <div className="content-intro">
+                                <h1 className="headline-lrg">Peerio Contacts</h1>
+                                <p>It looks like you haven't added any contacts yet. Click below to get started.</p>
+                                <Peerio.UI.Tappable element="div" className="btn-md" onTap={this.transitionTo.bind(this, 'contacts')}>
+                                  <i className="fa fa-user-plus"></i>&nbsp;Add a contact
+                                </Peerio.UI.Tappable>
+                                <img src="media/img/contacts.png"/>
+                              </div>;
+          contacts.push(intro_content);
+        };
+
       return (
         <div className="modal contact-select">
           <ul className="contact-list">
