ea3f17e81c893f1eba1fee93ff05a86987cfc616
Jan Drewniak <jan.drewniak@gmail.com>
2015-09-17 19:27:35 +0200

    adding icons to tab bar

--- jsx/tabbar.jsx
+++ jsx/tabbar.jsx
@@ -14,15 +14,19 @@
         <div id="tabbar">
           <Peerio.UI.TabBarButton text="Messages" active={tab === 'messages'}
                                   onActivate={this.transitionTo.bind(this, 'messages')}
+                                  icon="comments-o"
                                   showBadge={false}/>
 
           <Peerio.UI.TabBarButton text="Files" active={tab === 'files'}
                                   onActivate={this.transitionTo.bind(this, 'files')}
+                                  icon="files-o"
                                   showBadge={false}/>
 
           <Peerio.UI.TabBarButton text="Contacts" active={tab === 'contacts'}
                                   onActivate={this.transitionTo.bind(this, 'contacts')}
+                                  icon="users"
                                   showBadge={false}/>
+
         </div>
       );
     }
@@ -41,6 +45,7 @@
 
       return (
         <div ref="tab" className={tabClasses} onTouchStart={this.props.onActivate}>
+          <i className={'tab-icon fa fa-'+this.props.icon}></i>
           {this.props.text}
           <i className={badgeClasses}></i>
         </div>
