e221ba26a094c1bdaef4626e29bb947ab90bb498
Jan Drewniak <jan.drewniak@gmail.com>
2015-09-25 18:05:27 +0200

    removing link to add/import contact page until functional.

--- www/js/jsx/contacts.jsx
+++ www/js/jsx/contacts.jsx
@@ -16,7 +16,13 @@
       //TODO: add success/fail alert.
     },
     handleAddContact: function () {
-      this.transitionTo('add_contact');
+      //add contact prompt
+      Peerio.Action.showPrompt({headline: 'Add Contact',
+        text: 'Please enter username of the contact you want to add',
+        onAccept: this.addContactCallback
+      });
+      //Transition to add/import contact screen. 
+      //this.transitionTo('add_contact');
     },
     openContactView: function(id){
       this.transitionTo('contact',{id:id})
