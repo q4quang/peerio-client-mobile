783560b9659f81e4529486d7b32d7fca14c6c15c
Samvel Avanesov <seavan@gmail.com>
2015-11-20 05:36:05 +0300

    adding and confirming addresses (state update)

--- www/js/jsx/settings_account.jsx
+++ www/js/jsx/settings_account.jsx
@@ -1,6 +1,45 @@
 (function () {
 
     'use strict';
+    Peerio.UI.AccountSettingsItem = React.createClass({
+        getInitialState: function() {
+            return {
+                isPrimary: this.props.data.isPrimary
+            }
+        },
+
+        setPrimaryAddress: function(address) {
+            this.setState({isPrimary: true});
+            this.props.setPrimaryAddress(address);
+        },
+
+        render: function() {
+            var address = this.props.data;
+            var index = this.props.id;
+            var removeAddress = this.props.removeAddress;
+
+            return <div>
+                     <div>
+                        <div className="col-8">
+                            <span className="text-mono">{address.value}</span>
+                        </div>
+                        <div className="col-2 text-center" onClick={this.setPrimaryAddress.bind(this, address.value)}>
+                            <input type="radio"
+                                   name="address_default"
+                                   className="sr-only radio-button"
+                                   checked={this.state.isPrimary}
+                                />
+                            <label htmlFor={"address_default_"+index}
+                                   className="radio-label"></label>
+                        </div>
+                        <div className="col-2 text-center">
+                            <i className="fa fa-trash-o" onClick={removeAddress.bind(this, address.value)}/>
+                        </div>
+                    </div>
+
+                    </div>;
+        }
+    });
 
     Peerio.UI.AccountSettings = React.createClass({
         getInitialState: function(){
@@ -8,7 +47,8 @@
                 user: (Peerio.user.isMe) ? Peerio.user : false,
                 newAddressText: "",
                 firstName: Peerio.user ? Peerio.user.settings.firstName : '',
-                lastName: Peerio.user ? Peerio.user.settings.lastName : ''
+                lastName: Peerio.user ? Peerio.user.settings.lastName : '',
+                addresses: Peerio.user ? Peerio.user.getAddresses() : ''
             };
         },
         updateName: function(){
@@ -28,32 +68,43 @@
             this.updateName();
         },
 
-        addAddress: function(){
-
-        },
         onAddressChange: function(event){
             this.setState({newAddressText:event.target.value})
         },
 
         confirmAddress: function(address, code) {
+            var self = this;
             this.state.user.confirmAddress(address, code).then(
                 function() {
+                    self.setState({addresses: Peerio.user.getAddresses() });
                     Peerio.Action.showAlert({text: "Address authorized"})
                 });
         },
 
-        addNewAddress: function(){
+        removeAddress: function(address) {
+            var self = this;
+            this.state.user.removeAddress(address).then(function() {
+                self.setState({addresses: Peerio.user.getAddresses() });
+                Peerio.Action.showAlert({text: "Address removed"})
+            });
+        },
+
+        setPrimaryAddress: function(address) {
+            this.state.user.setPrimaryAddress(address);
+        },
+
+        addNewAddress: function() {
             //TODO: valid email or phone number.
             var newAddress = this.state.newAddressText;
             var emailRegex = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i);
             var phoneRegex = new RegExp(/^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/i);
             var self = this;
             if (emailRegex.test(newAddress)){
-                //mocking new address addition
                 var user = this.state.user;
                 user.validateAddress(newAddress)
                     .then(function(response) {
                         user.addAddress(newAddress).then( function() {
+                            self.setState({addresses: Peerio.user.getAddresses() });
                             self.setState({user:user, newAddressText:""})
                             Peerio.Action.showPrompt({
                                 headline: "Enter the code",
@@ -75,41 +126,8 @@
         },
         render: function(){
             var user = this.state.user;
-            var addresses = user.getAddresses();
-
-            addresses = addresses.map(function(address, index){
-                return (<div>
-                        {address.isConfirmed ? (
-                            <div>
-                                <div className="col-8">
-                                    <span className="text-mono">{address.value}</span>
-                                </div>
-                                <div className="col-2 text-center">
-                                    <input type="radio"
-                                           name="default_address"
-                                           id={"address_default_"+index}
-                                           className="sr-only radio-button"
-                                           checked={address.isPrimary}/>
-                                    <label htmlFor={"address_default_"+index}
-                                           className="radio-label"></label>
-                                </div>
-                                <div className="col-2 text-center">
-                                    <i className="fa fa-trash-o"/>
-                                </div>
-                            </div>
-                        ) :
-                            <div>
-                                <div className="col-8">
-                                    <span className="text-mono">{address.value}</span>
-                                </div>
-                                <div className="col-4 text-center">
-                                    <input type="text" className="text-input" placeholder="confirm"/>
-                                </div>
-                            </div>
-                        }
-                    </div>
-                );
-            });
+            var addresses = this.state.addresses;
+            var self = this;
 
             return (
                 <div className="content-padded flex-col">
@@ -141,7 +159,12 @@
                         <div className="info-label col-8">Addresses</div>
                         <div className="subhead-inline col-2">primary</div>
                         <div className="subhead-inline col-2">remove</div>
-                        {addresses}
+                        {
+                            addresses.map(function(address, index){
+                                return <Peerio.UI.AccountSettingsItem key={index} id={index} data={address}
+                                                                      removeAddress={self.removeAddress}
+                                                                      setPrimaryAddress={self.setPrimaryAddress}/>;
+                        })}
                         <div>
                             <div className="col-8">
                                 <input type="text" className="text-input" placeholder="add phone or email" onChange={this.onAddressChange} value={this.state.newAddressText}/>
