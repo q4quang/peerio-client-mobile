8682cac25fe0adc089447a1567fb83ef7486b4c1
Samvel Avanesov <seavan@gmail.com>
2015-12-10 16:18:22 +0300

    Adding 2FA handling and UI

--- src/js/jsx/app.jsx
+++ src/js/jsx/app.jsx
@@ -1,35 +1,43 @@
 (function () {
-  'use strict';
-  // Main component for app in authenticated state
-  Peerio.UI.App = React.createClass({
-    mixins: [ReactRouter.Navigation, ReactRouter.State, Peerio.UI.Mixins.RouteTools],
-    componentWillMount: function(){
-      this.subscriptions = [
-        Peerio.Dispatcher.onHardBackButton(this.handleHardwareBack),
-        Peerio.Dispatcher.onTransitionTo(this.handleTransition)
-      ];
-    },
-    componentWillUnmount: function(){
-      Peerio.Dispatcher.unsubscribe(this.subscriptions);
-    },
-    handleHardwareBack: function(){
-      if(this.isAppRoot()) return;
-      this.goBack();
-    },
-    // hack to allow out of router context components to navigate
-    handleTransition: function(){
-      this.transitionTo.apply(this, arguments);
-    },
-    render: function () {
-      return (
-        <div>
-          <Peerio.UI.NavBar/>
-          <Peerio.UI.SideBar/>
-          <RouteHandler/>
-          <Peerio.UI.Footer/>
-        </div>
-      );
-    }
-  });
+    'use strict';
+    // Main component for app in authenticated state
+    Peerio.UI.App = React.createClass({
+        mixins: [ReactRouter.Navigation, ReactRouter.State, Peerio.UI.Mixins.RouteTools],
+        componentWillMount: function(){
+            this.subscriptions = [
+                Peerio.Dispatcher.onHardBackButton(this.handleHardwareBack),
+                Peerio.Dispatcher.onTransitionTo(this.handleTransition),
+                Peerio.Dispatcher.onTwoFactorAuthRequested(this.handle2FA)
+            ];
+        },
+        componentWillUnmount: function(){
+            Peerio.Dispatcher.unsubscribe(this.subscriptions);
+        },
+        handleHardwareBack: function(){
+            if(this.isAppRoot()) return;
+            this.goBack();
+        },
+        // hack to allow out of router context components to navigate
+        handleTransition: function(){
+            this.transitionTo.apply(this, arguments);
+        },
+        handle2FA: function(cachedRequest) {
+            L.info('2fa requested');
+            L.info(cachedRequest);
+            // TODO: this is awful, need to find a better way
+            Peerio.user.cachedRequest = cachedRequest;
+            this.transitionTo('/2fa');
+        },
+        render: function () {
+            return (
+                <div>
+                    <Peerio.UI.NavBar/>
+                    <Peerio.UI.SideBar/>
+                    <RouteHandler/>
+                    <Peerio.UI.Footer/>
+                </div>
+            );
+        }
+    });
 
 }());
