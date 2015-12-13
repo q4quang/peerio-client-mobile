4c16d6b69a90f27955a128f39effd59235abab32
Anri Asaturov <anri82@gmail.com>
2015-12-13 22:27:18 +0200

    Sync, background connection logic, package updates

--- src/js/jsx/modal/sync.jsx
+++ src/js/jsx/modal/sync.jsx
@@ -7,7 +7,7 @@
 
     Peerio.UI.Sync = React.createClass({
         getInitialState: function () {
-            return {progressValue: 0, message: 'starting...'};
+            return {progressValue: 0, current: 0, max: 0, message: 'starting...', doRender: false};
         },
         componentWillMount: function () {
             this.subscriptions = [
@@ -18,6 +18,8 @@
         updateProgress: function (current, max, message) {
             var perc = max / 100.0;
             this.setState({
+                current: current,
+                max: max,
                 progressValue: perc ? (current / perc) + '%' : 0,
                 message: message
             });
@@ -26,6 +28,15 @@
             Peerio.Dispatcher.unsubscribe(this.subscriptions);
         },
         render: function () {
+            if (!this.state.doRender) {
+                window.setTimeout(()=> {
+                    if (!this.isMounted()) return;
+                    this.setState({doRender: true});
+                }, 2000);
+                return null;
+            }
+            var progressDetails = this.state.max > 1 ? '(' + this.state.current + '/' + this.state.max + ')' : '';
+
             var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
             return (
                 <ReactCSSTransitionGroup transitionName="fade" transitionAppear={true} transitionAppearTimeout={250}>
@@ -44,9 +55,10 @@
                         <div className="sync-stage">
                         </div>
 
-                        <div className="sync-progress-caption">{this.state.message}</div>
+                        <div className="sync-progress-caption">{this.state.message} {progressDetails}</div>
                         <div className="sync-progress">
-                            <div className="sync-progress-bar" style={{width: this.state.progressValue}}>
+                            <div className="sync-progress-bar"
+                                 style={{transition: this.state.progressValue=='0%'?'none':'width 500ms ease', width: this.state.progressValue }}>
                             </div>
                         </div>
                     </div>
