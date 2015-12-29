bdf4f937ded176f9f832a87dca4922759d9e43fe
Anri Asaturov <anri82@gmail.com>
2015-12-29 23:58:03 +0200

    Fail-safe native api facade

--- src/js/native_api.js
+++ src/js/native_api.js
@@ -18,7 +18,7 @@ Peerio.NativeAPI.init = function () {
     var cordova = window.cordova;
     var safariView = window.SafariViewController;
 
-    // Hardware/OS event handlers
+    //-- SYSTEM EVENT HANDLERS -----------------------------------------------------------------------------------------
     document.addEventListener('pause', Peerio.Action.pause, false);
     document.addEventListener('resume', Peerio.Action.resume, false);
     document.addEventListener('backbutton', Peerio.Action.hardBackButton, true);
@@ -31,188 +31,257 @@ Peerio.NativeAPI.init = function () {
     window.addEventListener('keyboardWillHide', Peerio.Action.keyboardWillHide, true);
     window.addEventListener('keyboardDidHide', Peerio.Action.keyboardDidHide, true);
 
-
-
-    /**
-     * Opens url in inAppBrowser
-     * @param url
-     */
-    api.openInBrowser = function (url) {
-        var open = cordova.InAppBrowser.open || window.open;
-
-        if (!safariView) {
-            open && open(url, '_blank', 'location=yes');
-            return;
-        }
-
-        safariView.isAvailable(function (available) {
-            if (!available) {
-                open && open(url, '_blank', 'location=yes');
-                return;
-            }
-            safariView.show({'url': url, 'enterReaderModeIfAvailable': false},
-                function (msg) {}, // success callbakc
-                function (msg) {
-                    L.error(msg);
-                });
-        });
-    };
-
-
+    //-- PLUGIN AVAILABILITY TESTS -------------------------------------------------------------------------------------
+    // we need separate tests because cordova plugins are not using the same (window.cordova.plugins)namespace
+    // and in case of separate plugins fail to load (?) we isolate them this way
     function isKeyboardPluginAvailable() {
         if (window.Keyboard) return true;
         L.info('Native API: Keyboard plugin not available.');
         return false;
     }
 
-    /**
-     * Hide software keyboard
-     */
-    api.hideKeyboard = function () {
-        if (!isKeyboardPluginAvailable()) return;
-
-        Keyboard.hide();
-    };
-
-    /**
-     * For IOS only. Hides 'accessory bar' with 'next', 'previous' and 'done' buttons.
-     */
-    api.hideKeyboardAccessoryBar = function (hide) {
-        if (!isKeyboardPluginAvailable()) return;
-        Keyboard.hideFormAccessoryBar(hide);
-    };
-
-    /**
-     *  When keyboard is open, shrinks the webview instead of viewport
-     */
-    api.shrinkViewOnKeyboardOpen = function (shrink) {
-        if (!isKeyboardPluginAvailable()) return;
-        Keyboard.shrinkView(shrink);
-    };
-
-    ////
-    api.disableScrollingInShrinkView = function (disable) {
-        if (!isKeyboardPluginAvailable()) return;
-        Keyboard.disableScrollingInShrinkView(disable);
-    };
-
-    function isInsomniaPluginAvailable(){
-        if(window.plugins && window.plugins.insomnia) return true;
+    function isInsomniaPluginAvailable() {
+        if (window.plugins && window.plugins.insomnia) return true;
         L.info('Native API: Insomnia plugin not available.');
         return false;
     }
 
-    api.preventSleep = function () {
-        window.plugins.insomnia.keepAwake();
+    function isClipboardAvailable() {
+        if (cordova && cordova.plugins && cordova.plugins.clipboard) return true;
+        L.info('Native API: Clipboard plugin not available.');
+        return false;
+    }
+
+    function isCameraAvailable() {
+        if (navigator.camera) return true;
+        L.info('Native API: Camera plugin not available.');
+        return false;
+    }
+
+    function isPushNotificationAvailable() {
+        if (window.PushNotification) return true;
+        L.info('NativeAPI: Push notifications not available');
+        return false;
+    }
+
+    //-- INTERNAL BROWSER ----------------------------------------------------------------------------------------------
+    /**
+     * Opens url in safari or inAppBrowser, whatever is available
+     * @param url
+     */
+    api.openInBrowser = function (url) {
+        var open = cordova.InAppBrowser.open || window.open;
+        try {
+            if (!safariView) {
+                open && open(url, '_blank', 'location=yes');
+                return;
+            }
+
+            safariView.isAvailable(function (available) {
+                if (!available) {
+                    open && open(url, '_blank', 'location=yes');
+                    return;
+                }
+                safariView.show({'url': url, 'enterReaderModeIfAvailable': false},
+                    function (msg) {
+                    }, // success callback
+                    function (msg) {
+                        L.error(msg);
+                    });
+            });
+        } catch (ex) {
+            L.error('Failed to open url in browser. {0}', ex);
+        }
     };
 
-    initializers.preventSleep = function () {
-        if (window.plugins && window.plugins.insomnia) return;
 
-        return console.log.bind(console, getGenericMsg('preventSleep'));
-    };
-
-    api.allowSleep = function () {
-        window.plugins.insomnia.allowSleepAgain();
-    };
-
-    initializers.allowSleep = function () {
-        if (window.plugins && window.plugins.insomnia) return;
-
-        return console.log.bind(console, getGenericMsg('allowSleep'));
+    //-- KEYBOARD ------------------------------------------------------------------------------------------------------
+    /**
+     * Hide software keyboard
+     */
+    api.hideKeyboard = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.hide();
+        } catch (ex) {
+            L.error('Failed to hide keyboard. {0}', ex);
+        }
     };
 
     /**
+     * For IOS only. Hides 'accessory bar' with 'next', 'previous' and 'done' buttons when keyboard is open.
+     */
+    api.hideKeyboardAccessoryBar = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.hideFormAccessoryBar(true);
+        } catch (ex) {
+            L.error('Failed to hide keyboard accessory bar. {0}', ex);
+        }
+    };
+    /**
+     * For IOS only. Shows 'accessory bar' with 'next', 'previous' and 'done' buttons when keyboard is open.
+     */
+    api.showKeyboardAccessoryBar = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.hideFormAccessoryBar(false);
+        } catch (ex) {
+            L.error('Failed to show keyboard accessory bar. {0}', ex);
+        }
+    };
+
+    /**
+     *  Shrinks the webview instead of viewport when keyboard is open/
+     */
+    api.shrinkViewOnKeyboardOpen = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.shrinkView(true);
+        } catch (ex) {
+            L.error('Failed to change view shrink settings. {0}', ex);
+        }
+    };
+
+    /**
+     *  Shrinks the viewport instead of webview when keyboard is open.
+     */
+    api.doNotShrinkViewOnKeyboardOpen = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.shrinkView(false);
+        } catch (ex) {
+            L.error('Failed to change view shrink settings. {0}', ex);
+        }
+
+    };
+
+    api.enableScrollingInShrinkView = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.disableScrollingInShrinkView(false);
+        } catch (ex) {
+            L.error('Failed to change shrink scroll settings. {0}', ex);
+        }
+
+    };
+
+    api.disableScrollingInShrinkView = function () {
+        try {
+            isKeyboardPluginAvailable() && Keyboard.disableScrollingInShrinkView(true);
+        } catch (ex) {
+            L.error('Failed to change shrink scroll settings. {0}', ex);
+        }
+    };
+
+    //-- DEVICE POWER MANAGEMENT ---------------------------------------------------------------------------------------
+    /**
+     * Prevents smartphone sleep if idle while app is open
+     */
+    api.preventSleep = function () {
+        try {
+            isInsomniaPluginAvailable() && window.plugins.insomnia.keepAwake();
+        } catch (ex) {
+            L.error('Failed to prevent sleep. {0}', ex);
+        }
+
+    };
+
+    /**
+     * Allows smartphone to sleep if idle while app is open
+     */
+    api.allowSleep = function () {
+        try {
+            isInsomniaPluginAvailable() && window.plugins.insomnia.allowSleepAgain();
+        } catch (ex) {
+            L.error('Failed to allow sleep. {0}', ex);
+        }
+
+    };
+
+    //-- DEVICE INFORMATION --------------------------------------------------------------------------------------------
+    /**
      * Get app version from config.xml
-     * @param {function(string)} callback
      */
     api.getAppVersion = function () {
         return window.AppVersion && AppVersion.version || 'n/a';
     };
 
+    //-- CAMERA --------------------------------------------------------------------------------------------------------
     /**
      * Takes picture from camera or photo library
      * @param {bool} camera - set true to take a new picture instead of picking from the library
      * @returns {Promise<string>} - file url
      */
     api.takePicture = function (camera) {
+        if (!isCameraAvailable()) return Promise.reject();
+
         return new Promise(function (resolve, reject) {
-            navigator.camera.getPicture(resolve, reject, { // please, don't change properties without (re)reading docs on them first and testing result after
-                    // yes, Anri, especially you!
-                    sourceType: camera ? Camera.PictureSourceType.CAMERA : Camera.PictureSourceType.PHOTOLIBRARY,
-                    destinationType: Camera.DestinationType.FILE_URI,
-                    encodingType: Camera.EncodingType.JPEG,
-                    mediaType: Camera.MediaType.ALLMEDIA,
-                    cameraDirection: Camera.Direction.BACK,
-                    allowEdit: false,
-                    correctOrientation: true,
-                    saveToPhotoAlbum: false,
-                    quality: 90
-                });
-        });
-    };
-
-    api.pluginsAvailable = function () {
-        return !(typeof cordova === 'undefined' || typeof cordova.plugins === 'undefined' || typeof cordova.plugins.clipboard === 'undefined');
-    };
-
-    api.copyToClipboard = function (text) {
-        return new Promise((resolve, reject) => {
-            if (!api.pluginsAvailable()) {
-                L.info('clipboard is unavailable on the platform');
-                reject('clipboard is unavailable on the platform');
-            } else {
-                var clipboard = cordova.plugins.clipboard;
-                clipboard.copy(text, function () {
-                    L.info('copied successfully');
-                    resolve('copied successfully');
-                }, function () {
-                    L.info('copy failed');
-                    reject('copy failed');
-                });
-            }
+            navigator.camera.getPicture(resolve, reject, {
+                // please, don't change properties without (re)reading docs on them first and testing result afterwards.
+                // yes, Anri, especially you!
+                sourceType: camera ? Camera.PictureSourceType.CAMERA : Camera.PictureSourceType.PHOTOLIBRARY,
+                destinationType: Camera.DestinationType.FILE_URI,
+                encodingType: Camera.EncodingType.JPEG,
+                mediaType: Camera.MediaType.ALLMEDIA,
+                cameraDirection: Camera.Direction.BACK,
+                allowEdit: false,
+                correctOrientation: true,
+                saveToPhotoAlbum: false,
+                quality: 90
+            });
         });
     };
 
     /**
+     * Removes all temporary pictures taken with previous cordova camera plugin calls
+     */
+    api.cleanupCamera = function () {
+        try {
+            isCameraAvailable() && navigator.camera.cleanup();
+        } catch (ex) {
+            L.error('Failed to cleanup camera. {0}', ex);
+        }
+    };
+
+
+    //-- CLIPBOARD -----------------------------------------------------------------------------------------------------
+    api.copyToClipboard = function (text) {
+        if (!isClipboardAvailable()) return Promise.reject();
+
+        return new Promise((resolve, reject) => cordova.plugins.clipboard.copy(text, resolve, reject));
+    };
+
+    //-- PUSH NOTIFICATIONS --------------------------------------------------------------------------------------------
+    /**
      * Enables push notifications (if possible on the platform)
-     * @returns {Promise<bool>} - whether enabling notifications was successful or not
+     * @returns {Promise}
      */
     api.enablePushNotifications = function () {
+        if (!isPushNotificationAvailable()) return Promise.reject();
+
+        L.info('Enabling push notifications.');
+
         return new Promise(function (resolve, reject) {
-            if (typeof PushNotification === 'undefined') {
-                L.info('push notifications are unavailable on the platform');
-                reject('push notifications are unavailable on the platform');
-            }
-            L.info('enabling push notifications');
             var push = PushNotification.init({
-                'ios': { 'alert': 'true', 'badge': 'true', 'sound': 'true' },
-                'android': { 'senderID': Peerio.Config.push.android.senderId }
+                'ios': {'alert': 'true', 'badge': 'true', 'sound': 'true'},
+                'android': {'senderID': Peerio.Config.push.android.senderId}
             });
+
+            api.push = push;
+
             push.on('registration', function (data) {
                 L.info('push notification reg.id: ' + data.registrationId);
-                if (window.device && window.device.platform) {
-                    var platform = window.device.platform.toLowerCase();
-                    var to_send = {};
-                    to_send[platform] = data.registrationId;
-                    Peerio.Net.registerMobileDevice(to_send);
-                    window.L.info(to_send);
-                    api.push = push;
-                    resolve(to_send);
-                }
+                var to_send = {};
+                to_send[Peerio.runtime.platform] = data.registrationId;
+                Peerio.Net.registerMobileDevice(to_send);
+                resolve();
+                L.info('push notifications enabled');
             });
+
             push.on('notification', function (data) {
-                L.info('push notification message: ' + data.message);
-                L.info('push notification title: ' + data.title);
-                L.info('push notification count: ' + data.count);
+                L.silly('push notification received: {0}', data);
             });
 
             push.on('error', function (e) {
-                L.info('push notification error: ' + e.message);
-                reject('push notification error: ' + e.message);
+                L.error('push notification error: {0}', e);
+                reject();
             });
 
-            L.info('push notifications enabled');
         });
     };
 
@@ -222,72 +291,38 @@ Peerio.NativeAPI.init = function () {
      */
     api.disablePushNotifications = function () {
         return new Promise(function (resolve, reject) {
-            if (api.push) {
-                api.push.unregister(function () {
-                    L.info('push unregister succeeded');
-                    resolve('unregister success');
-                }, function () {
-                    L.info('push unregister failed');
-                    reject('unregister failed');
-                });
-            } else {
-                resolve('no push notifications were enabled');
+            if (!api.push) {
+                resolve();
+                return;
             }
+
+            api.push.unregister(function () {
+                L.info('Push notifications disabled.');
+                resolve();
+            }, function () {
+                L.info('Failed to disable push notifications.');
+                reject();
+            });
         });
     };
 
     /**
-     * Sets badge number 
+     * Sets badge number
      */
-    api.setPushBadge = function(number) {
-        api.push && api.push.setApplicationIconBadgeNumber( () => {
-            L.info('push badge number set');
-        }, (e) => {
-            L.error('error setting push badge number');
-        }, number);
+    api.setPushBadge = function (number) {
+        api.push && api.push.setApplicationIconBadgeNumber(
+            () => L.info('push badge number set'),
+            e => L.error('error setting push badge number. {0}', e),
+            number);
     };
 
     /**
      * Clears badge
      */
-    api.clearPushBadge = function() {
+    api.clearPushBadge = function () {
         api.setPushBadge(0);
     };
 
-    initializers.takePicture = function () {
-        if (navigator.camera) return;
+    //------------------------------------------------------------------------------------------------------------------
 
-        return console.log.bind(console, getGenericMsg('takePicture'));
-    };
-
-    /**
-     * Removes all temporary pictures taken with previous cordova camera plugin calls
-     */
-    api.cleanupCamera = function () {
-        navigator.camera.cleanup();
-    };
-
-    initializers.cleanupCamera = function () {
-        if (navigator.camera) return;
-
-        return console.log.bind(console, getGenericMsg('cleanupCamera'));
-    };
-
-    /**
-     * It was like this in the existing code
-     */
-    api.signOut = function () {
-        window.location.reload();
-    };
-//--------------------------------------------------------------------------------------------------------------------
-    _.forOwn(initializers, function (fn, name) {
-        // if initializer returns alternative function
-        // (usually a mock that is safe to call)
-        // assign that function instead of original one
-        var alterFn = fn();
-        if (alterFn) api[name] = alterFn;
-    });
-    // cleaning memory
-    initializers = null;
-    //--------------------------------------------------------------------------------------------------------------------
 };
