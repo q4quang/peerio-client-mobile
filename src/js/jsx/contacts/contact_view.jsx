85331cf3f745cfb7f77f49ed2bb5628b6954b7c3
Anri Asaturov <anri82@gmail.com>
2016-01-02 12:12:46 +0200

    Fixing contact view

--- src/js/jsx/contacts/contact_view.jsx
+++ src/js/jsx/contacts/contact_view.jsx
@@ -10,66 +10,91 @@
                 Peerio.Dispatcher.onBigGreenButton(this.startConversationWithContact),
                 Peerio.Dispatcher.onContactsUpdated(this.forceUpdate.bind(this, null))];
         },
+
         startConversationWithContact: function () {
             this.transitionTo('new_message', {id: this.contact.username});
         },
+
         componentWillUnmount: function () {
             Peerio.Dispatcher.unsubscribe(this.subscriptions);
         },
+
         handleAccept: function () {
-            // TODO: there is probably a better way to invoke loading contacts
-            this.contact.accept().then(function() {
-                Peerio.user.loadContacts();
+            this.contact.accept().catch(function (ex) {
+                L.error('Failed to accept contact request. {0}', ex);
+                Peerio.Action.showAlert({text: 'Failed to accept contact request.'});
             });
 
-
         },
+
         handleReject: function () {
-            // TODO: there is probably a better way to invoke loading contacts
-            this.contact.reject().then(function() {
-                Peerio.user.loadContacts();
+            this.contact.reject().catch(function (ex) {
+                L.error('Failed to reject contact request. {0}', ex);
+                Peerio.Action.showAlert({text: 'Failed to reject contact request.'});
             });
         },
-        removeContactAndGoBack: function (username) {
-            this.contact.isRequest ? this.contact.cancelRequest() : this.contact.remove();
-            this.goBack();
+
+        removeContactAndGoBack: function () {
+            (this.contact.isRequest ? this.contact.cancelRequest() : this.contact.remove())
+                .then(()=> this.goBack())
+                .catch(function (ex) {
+                    L.error('Failed to remove contact. {0}', ex);
+                    Peerio.Action.showAlert({text: 'Failed to remove contact.'});
+                });
         },
+
         handleRemove: function () {
+            // asking confirmation for established contacts only
             if (!this.contact.isRequest) {
                 Peerio.Action.showConfirm({
                     headline: 'Remove Contact?',
                     text: 'Are you sure you want to remove ' + this.contact.username +
                     ' from contacts? You will not be able to message and share files with this contact after removal.',
-                    onAccept: this.removeContactAndGoBack.bind(this, this.contact.username)
+                    onAccept: this.removeContactAndGoBack
                 });
-            } else this.removeContactAndGoBack(this.contact.username);
+            } else this.removeContactAndGoBack();
         },
+
         render: function () {
             this.contact = Peerio.user.contacts.dict[this.props.params.id]
                 || Peerio.user.receivedContactRequests.dict[this.props.params.id]
                 || Peerio.user.sentContactRequests.dict[this.props.params.id];
-            if(!this.contact) {
+
+            if (!this.contact) {
                 this.goBack();
                 return null;
             }
-            var buttonNode = null, pendingNode = null;
-            if (this.contact.responsePending) {
-                pendingNode =
-                    <div className="pending"><i className="fa fa-spinner fa-pulse"></i> waiting for server response...
-                    </div>;
-            } else if (!this.contact.isMe) {
-                buttonNode = (
-                    <div>
-                        { this.contact.isRequest && this.contact.isReceivedRequest ?
-                            <Peerio.UI.Tappable element="div" className="btn-md btn-safe" onTap={this.handleAccept}>Accept
-                                contact request</Peerio.UI.Tappable> : null }
-                        { this.contact.isRequest && this.contact.isReceivedRequest ?
-                            <Peerio.UI.Tappable element="div" className="btn-md btn-danger" onTap={this.handleReject}>Reject
-                                contact request</Peerio.UI.Tappable>
-                            : <Peerio.UI.Tappable element="div" className="btn-md btn-danger" onTap={this.handleRemove}>Remove
-                            contact</Peerio.UI.Tappable>}
-                    </div>);
+
+            var buttonNode = this.contact.isMe ? null : (
+                <div>
+                    { this.contact.isReceivedRequest ? (
+                        <span>
+                                <Peerio.UI.Tappable element="div" className="btn-md btn-safe"
+                                                    onTap={this.handleAccept}>Accept contact request
+                                </Peerio.UI.Tappable>
+                                <Peerio.UI.Tappable element="div" className="btn-md btn-danger"
+                                                    onTap={this.handleReject}>Reject contact request
+                                </Peerio.UI.Tappable>
+                        </span>) : null
+                    }
+
+                    <Peerio.UI.Tappable element="div" className="btn-md btn-danger" onTap={this.handleRemove}>
+                        Remove contact
+                    </Peerio.UI.Tappable>
+                </div>
+            );
+
+            var status = 'Established contact.';
+
+            if(this.contact.isMe) {
+                status = 'This is you';
+            } else if (this.contact.isReceivedRequest) {
+                status = 'Pending: you received request';
+            } else if (this.contact.isRequest){
+                status = 'Pending: you sent request';
             }
+
+
             return (
                 <div className="content-padded contact-view">
 
@@ -92,9 +117,7 @@
 
                         <div className="info-block">
                             <div className="info-label">State:</div>
-                            <div className="col-12 info-content">{ this.contact.isMe ? 'This is you!' :
-                                (this.contact.isRequest ? 'Pending: ' + (this.contact.isReceivedRequest ? 'you received request.' : 'you sent request')
-                                    : 'Established contact')}</div>
+                            <div className="col-12 info-content">{status}</div>
                         </div>
 
                         <div className="info-block">
@@ -104,7 +127,7 @@
 
                     </div>
 
-                    {pendingNode || buttonNode}
+                    {buttonNode}
 
                 </div>
             );
