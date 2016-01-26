69c3d0787d5c568fac5dc9fa16b8df7c6ef4d824
Samvel Avanesov <seavan@gmail.com>
2016-01-26 18:47:03 +0100

    adding invite codes to add contact view

--- src/js/jsx/contacts/contact_add.jsx
+++ src/js/jsx/contacts/contact_add.jsx
@@ -3,15 +3,28 @@
 
 Peerio.UI.AddContact = React.createClass({
   mixins: [ReactRouter.Navigation],
-  getInitialState: function(){
-    return {searchString: ''};
+
+  componentWillMount: function() {
+      Peerio.user.getInviteCode()
+      .then( (code) => {
+          code && code.inviteCode && this.setState( { inviteCode: code.inviteCode } );
+      });
   },
+
+  getInitialState: function() {
+      return { 
+          searchString: '',
+      };
+  },
+
   goToSearch: function(){
     this.transitionTo('add_contact_search', {id:this.state.searchString});
   },
+
   updateSearchString: function(e){
     this.setState({searchString: e.target.value});
   },
+
   render: function(){
     var searchButton = (this.state.searchString.length === 0) ?
         <div className="btn-disabled"><i className="material-icons">search</i> Search</div>
@@ -21,6 +34,7 @@ Peerio.UI.AddContact = React.createClass({
     return  (<div className="content without-tab-bar">
       <div className="flex-col flex-justify-start">
         <div className="headline">Add Contact</div>
+        <p>{this.state.inviteCode}</p>
         <div  className="input-group">
           <input type="text" onChange={this.updateSearchString} placeholder="Search by username, email or phone" value={this.state.searchString}/>
         </div>
