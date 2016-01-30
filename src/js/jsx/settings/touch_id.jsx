4a3472f58da911e85439eb1619b10366e7f0a712
Anri Asaturov <anri82@gmail.com>
2016-01-30 06:16:58 +0200

    Fix unhandled rejection

--- src/js/jsx/settings/touch_id.jsx
+++ src/js/jsx/settings/touch_id.jsx
@@ -2,158 +2,164 @@
     'use strict';
     Peerio.UI.TouchId = React.createClass({
         statics: {
-            keyname: function(username) {
+            keyname: function (username) {
                 return (username ? username : Peerio.user.username) + '_keypair';
             },
 
-            touchidname: function(username) {
+            touchidname: function (username) {
                 return (username ? username : Peerio.user.username) + '_touchid';
             },
 
-            bubblename: function() {
+            bubblename: function () {
                 return Peerio.user.username + '_touch_bubble';
             },
 
-            offername: function() {
+            offername: function () {
                 return Peerio.user.username + '_touch_offer';
             },
 
-            hasTouchID: function(username) {
+            hasTouchID: function (username) {
                 return Peerio.TinyDB.getObject(Peerio.UI.TouchId.touchidname(username));
             },
 
-            setHasTouchID: function(username, value) {
+            setHasTouchID: function (username, value) {
                 return value ?
                     Peerio.TinyDB.setObject(Peerio.UI.TouchId.touchidname(username), true) :
                     Peerio.TinyDB.removeItem(Peerio.UI.TouchId.touchidname(username));
             },
 
-            hasUserSeenOffer: function() {
+            hasUserSeenOffer: function () {
                 return Peerio.TinyDB.getObject(Peerio.UI.TouchId.offername());
             },
 
-            setUserSeenOffer: function() {
+            setUserSeenOffer: function () {
                 return Peerio.TinyDB.setObject(Peerio.UI.TouchId.offername(), true);
             },
 
-            hasUserSeenBubble: function() {
+            hasUserSeenBubble: function () {
                 return Peerio.TinyDB.getObject(Peerio.UI.TouchId.bubblename());
             },
 
-            setUserSeenBubble: function() {
+            setUserSeenBubble: function () {
                 return Peerio.TinyDB.setObject(Peerio.UI.TouchId.bubblename(), true);
             },
 
-            showOfferIfNeeded: function() {
+            showOfferIfNeeded: function () {
                 Peerio.UI.TouchId.isFeatureAvailable()
-                .then( () => Peerio.UI.TouchId.hasUserSeenOffer() )
-                .then( (value) => {
-                    if(value) return Promise.resolve(false);
-                    Peerio.UI.Confirm.show({
-                        text: 'Would you like to enable Touch ID?',
-                        caption: 'Touch ID requires using your Apple Keychain'
-                    })
-                    .then( () => Peerio.UI.TouchId.saveKeyPair() )
-                    .catch( () => true )
-                    .then( () => Peerio.UI.TouchId.setUserSeenOffer() );
-
-                    return Promise.resolve(true);
-                });
-            },
-
-            showExclamationBubble: function() {
-                return Peerio.UI.TouchId.hasUserSeenBubble()
-                .then( (hasSeen) => {
-                    return hasSeen ? Promise.resolve(true) :
+                    .then(() => Peerio.UI.TouchId.hasUserSeenOffer())
+                    .then((value) => {
+                        if (value) return Promise.resolve(false);
                         Peerio.UI.Confirm.show({
-                        text: 'Enabling TouchID requires using your keychain, would you like to proceed?'
+                                text: 'Would you like to enable Touch ID?',
+                                caption: 'Touch ID requires using your Apple Keychain'
+                            })
+                            .then(() => Peerio.UI.TouchId.saveKeyPair())
+                            .catch(() => true)
+                            .then(() => Peerio.UI.TouchId.setUserSeenOffer());
+
+                        return Promise.resolve(true);
+                    })
+                    .catch((err)=> {
+                        L.verbose(err);
                     });
-                })
-                .then( () => this.setUserSeenBubble() );
             },
 
-            getSystemPin: function(username) {
+            showExclamationBubble: function () {
+                return Peerio.UI.TouchId.hasUserSeenBubble()
+                    .then((hasSeen) => {
+                        return hasSeen ? Promise.resolve(true) :
+                            Peerio.UI.Confirm.show({
+                                text: 'Enabling TouchID requires using your keychain, would you like to proceed?'
+                            });
+                    })
+                    .then(() => this.setUserSeenBubble());
+            },
+
+            getSystemPin: function (username) {
                 return window.PeerioTouchIdKeychain.getValue(
-                Peerio.UI.TouchId.keyname(username));
+                    Peerio.UI.TouchId.keyname(username));
             },
 
-            enableTouchId: function() {
+            enableTouchId: function () {
                 return Peerio.UI.TouchId.clearKeyPair()
-                .catch( (error) => L.info(error) )
-                .then( () => Peerio.UI.TouchId.showExclamationBubble() )
-                .then( () => Peerio.UI.TouchId.saveKeyPair());
+                    .catch((error) => L.info(error))
+                    .then(() => Peerio.UI.TouchId.showExclamationBubble())
+                    .then(() => Peerio.UI.TouchId.saveKeyPair());
             },
 
-            saveKeyPair: function() {
+            saveKeyPair: function () {
                 var systemPin = uuid.v4();
                 return window.PeerioTouchIdKeychain.saveValue(
                     Peerio.UI.TouchId.keyname(),
                     systemPin
-                )
-                .then( () => Peerio.user.setPIN(systemPin, true) )
-                .catch( (error) => {
-                    Peerio.UI.TouchId.setHasTouchID(Peerio.user.username, false);
-                    return Promise.reject(error);
-                })
-                .then( () => Peerio.UI.TouchId.setHasTouchID(Peerio.user.username, true) );
+                    )
+                    .then(() => Peerio.user.setPIN(systemPin, true))
+                    .catch((error) => {
+                        Peerio.UI.TouchId.setHasTouchID(Peerio.user.username, false);
+                        return Promise.reject(error);
+                    })
+                    .then(() => Peerio.UI.TouchId.setHasTouchID(Peerio.user.username, true));
             },
 
-            clearKeyPair: function() {
+            clearKeyPair: function () {
                 return window.PeerioTouchIdKeychain.deleteValue(
                     Peerio.UI.TouchId.keyname())
-                    .then( () => Peerio.UI.TouchId.setHasTouchID(Peerio.user.username, false) );
+                    .then(() => Peerio.UI.TouchId.setHasTouchID(Peerio.user.username, false));
             },
 
-            isFeatureAvailable: function() {
+            isFeatureAvailable: function () {
                 return window.PeerioTouchIdKeychain ?
                     window.PeerioTouchIdKeychain.isFeatureAvailable() : Promise.reject(false);
             }
         },
 
-        getInitialState: function() {
-            return { visible: false, enabled: false };
+        getInitialState: function () {
+            return {visible: false, enabled: false};
         },
 
-        componentWillMount: function() {
+        componentWillMount: function () {
             Peerio.UI.TouchId.isFeatureAvailable()
-            .then( () => {
-                this.setState( { visible: true } );
+                .then(() => {
+                    this.setState({visible: true});
 
-                Peerio.UI.TouchId.hasTouchID(Peerio.user.username)
-                .then( (value) => this.setState({enabled: !!value}) );
+                    Peerio.UI.TouchId.hasTouchID(Peerio.user.username)
+                        .then((value) => this.setState({enabled: !!value}));
 
-                Peerio.NativeAPI.isForcefulFingerprintEnabled()
-                .then( (value) => this.setState({fingerPrintWarning: value}) );
-            });
-       },
+                    Peerio.NativeAPI.isForcefulFingerprintEnabled()
+                        .then((value) => this.setState({fingerPrintWarning: value}));
+                });
+        },
 
-        enableTouchId: function() {
+        enableTouchId: function () {
             var enabled = !this.state.enabled;
             enabled ? Peerio.UI.TouchId.enableTouchId()
-            .then( () => {
-                this.setState({enabled: true});
-            }) : Peerio.UI.TouchId.clearKeyPair()
-            .then( () => {
-                this.setState({enabled: false});
-            });
+                .then(() => {
+                    this.setState({enabled: true});
+                }) : Peerio.UI.TouchId.clearKeyPair()
+                .then(() => {
+                    this.setState({enabled: false});
+                });
         },
 
-        render: function() {
+        render: function () {
 
             return this.state.visible ? (
                 <ul>
-                  <Peerio.UI.Tappable element="li" onTap={this.enableTouchId}>
-                    <div type="checkbox" className={'checkbox-input' + (this.state.enabled
+                    <Peerio.UI.Tappable element="li" onTap={this.enableTouchId}>
+                        <div type="checkbox" className={'checkbox-input' + (this.state.enabled
                     ? ' checked': '')}>
-                        <i className="material-icons"></i>
+                            <i className="material-icons"></i>
                         </div>
                         <div>Enable fingerprint identification</div>
                     </Peerio.UI.Tappable>
                     { this.state.fingerPrintWarning ? (
-                    <div>
-                        <p><b>NOTE ON LAW ENFORCEMENT</b></p>
-                        <p>In Oct. 2014, a USA court ruled that a police officer can demand you to unlock your device with a fingerprint but not an alphanumeric passcode. Similar laws may exist in other national or regional jurisdictions and should be considered if law enforcement is part of your threat model.</p>
-                    </div>
+                        <div>
+                            <p><b>NOTE ON LAW ENFORCEMENT</b></p>
+                            <p>In Oct. 2014, a USA court ruled that a police officer can demand you to unlock your
+                                device with a fingerprint but not an alphanumeric passcode. Similar laws may exist in
+                                other national or regional jurisdictions and should be considered if law enforcement is
+                                part of your threat model.</p>
+                        </div>
                     ) : null }
                 </ul>
             ) : (
